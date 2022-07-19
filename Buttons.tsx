4b0decba986feb700707e1bdc5547c0a08e9c405
Guarlic <hyunujung0419@gmail.com>
2022-07-19 21:09:48 +0900

    Update Buttons (remove root)

--- Buttons.tsx
+++ Buttons.tsx
@@ -1,24 +1,14 @@
 import * as React from 'react';
 import './style.css';
-import { root } from './index';
 import { count } from './App';
 
 export default function Buttons() {
   const Button1 = () => (
-    <button
-      onClick = { () => (
-        count++,
-        alert(count)
-      )}
-    >test</button>
+    <button onClick={() => (count++, alert(count))}>test</button>
   );
-  
+
   const Button2 = () => (
-    <button
-      onClick = { () => (
-        alert('This is Button2!!')
-      )}
-    >button2</button>
+    <button onClick={() => alert('This is Button2!!')}>button2</button>
   );
 
   return (
